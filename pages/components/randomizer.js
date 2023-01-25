db9f217d9c58eab9bc4cbe3a6cf66b88aa619154
ljlabitag <ljlabitagdev@gmail.com>
2023-01-25 11:08:33 +0800

    Added random number generator

---
+++ pages/components/randomizer.js
@@ -0,0 +1,15 @@
+//generate random numbers for Megalotto 6/45
+export default function Randomizer() {
+    const randomNumbers = [];
+    
+    while (randomNumbers.length <= 6) {
+        const randomNumber = Math.floor(Math.random()*45) + 1;
+        randomNumbers.push(randomNumber);
+    }
+
+    return (
+        randomNumbers.map((lottoNumber, index) => {
+            return <li key={index}>{lottoNumber}</li>
+        })
+    );
+}
